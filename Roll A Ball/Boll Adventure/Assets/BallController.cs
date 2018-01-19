13d645d332f4993410cf3204791d5fce06e3c894
Marowins <gamemaro@naver.com>
2018-01-19 21:35:05 +0900

    Ball Adventure!

---
+++ Roll A Ball/Boll Adventure/Assets/BallController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour {
+
+	public float speed = 5f;
+	public float jumpForce = 250f;
+	public Transform SpawnPoint;
+
+	Vector3 movement;
+
+	bool isJumping;
+	Rigidbody rb;
+
+	void Awake(){
+		rb = GetComponent<Rigidbody> ();
+	}
+
+	void FixedUpdate(){
+		Move ();
+		if (Input.GetButtonDown ("Jump"))
+			isJumping = true;
+	}
+
+	void Move(){
+		float x = Input.GetAxisRaw ("Horizontal");
+		float z = Input.GetAxisRaw ("Vertical");
+
+		movement = new Vector3 (x, 0f, z) * speed;
+		if (isJumping) {
+			isJumping = false;
+			if (movement.y <= 0.5) {
+				movement.y = jumpForce;
+			}
+		}
+			
+		rb.AddForce (movement);
+	}
+
+	void OnTriggerEnter(Collider other){
+		string otherTag = other.gameObject.tag;
+		if (otherTag == "DeadLine")
+			transform.position = SpawnPoint.position;
+		else if (otherTag == "Score")
+			other.gameObject.SetActive (false);
+		else if (otherTag == "Death")
+			Destroy (this.gameObject);
+		else if (otherTag == "Clear")
+			Debug.Log ("Score");
+	}
+		
+}
